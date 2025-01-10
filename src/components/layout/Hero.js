8e3cf2e565feee899597c31682bdcd4d882e2428
LeoGuimas <gleo.dev0@gmail.com>
2025-01-10 15:16:15 -0300

    Refactor icon structure; add HomeMenu component and update Hero content

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Right from "./icons/Right";
+import Right from "../icons/Right";
 
 export default function Hero() {
   return (
@@ -9,8 +9,8 @@ export default function Hero() {
           Tudo é melhor <br /> com uma&nbsp;
           <span className="text-primary">Pizza</span>
         </h1>
-        <p className="my-4 text-gray-500">
-          A resposta é pizza. Não importa a pergunta!
+        <p className="my-6 text-gray-500">
+        Nada como uma pizza quentinha para alegrar o dia! Seja clássica ou recheada de sabores inusitados, cada fatia é um convite ao prazer. Peça a sua e aproveite! 🍕
         </p>
         <div className="flex gap-4 text-sm">
           <button className="bg-primary flex items-center gap-2 rounded-full px-4 py-2 text-white">
